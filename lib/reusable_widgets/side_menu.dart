6927a426013da3868bed741524d34bf77598752b
alex <alexstevenn19@gmail.com>
2023-03-21 11:32:13 +0800

    Side Bar Revision 1: Log Out function, drop down settings

--- lib/reusable_widgets/side_menu.dart
+++ lib/reusable_widgets/side_menu.dart
@@ -1,23 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:travel_trip_application/screens/signin_screen.dart';
 
 class NavDrawer extends StatelessWidget {
-
+  const NavDrawer({super.key});
   @override
   Widget build(BuildContext context) {
     return Drawer(
       child: ListView(
         padding: EdgeInsets.zero,
         children: <Widget>[
-          const UserAccountsDrawerHeader(
-            accountName: Text("Username"),
-            accountEmail: Text("Country"),
-            currentAccountPicture: CircleAvatar(
-              backgroundImage: NetworkImage(
-                  "https://randomuser.me/api/portraits/men/47.jpg"),
-            ),
-            decoration: BoxDecoration(
+          DrawerHeader(
+            decoration: const BoxDecoration(
               color: Colors.green,
             ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: const <Widget>[
+                CircleAvatar(
+                  radius: 30,
+                  backgroundImage: NetworkImage("https://randomuser.me/api/portraits/men/47.jpg"),
+                ),
+                SizedBox(height: 10),
+                Text(
+                  'John Doe',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                  ),
+                ),
+                SizedBox(height: 5),
+                Text(
+                  'USA',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 14,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          ListTile(
+            leading: const Icon(Icons.person_rounded),
+            title: const Text('Profile'),
+            onTap: () =>{},
           ),
           ListTile(
             leading: const Icon(Icons.map),
@@ -32,17 +57,44 @@ class NavDrawer extends StatelessWidget {
           ListTile(
             leading: const Icon(Icons.notifications),
             title: const Text('Notification'),
-            onTap: () => {Navigator.of(context).pop()},
+            onTap: () => {Navigator.of(context).pop()}, //go back to screen
           ),
-          ListTile(
-            leading: const Icon(Icons.border_color),
-            title: const Text('Feedback'),
-            onTap: () => {Navigator.of(context).pop()},
+          ExpansionTile(
+            leading: const Icon(Icons.settings),
+            title: const Text('Settings'),
+            children: <Widget>[
+              ListTile(
+                leading: const Icon(Icons.language),
+                title: const Text('Language'),
+                onTap: () => {Navigator.of(context).pop()},
+              ),
+              ListTile(
+                leading: const Icon(Icons.dark_mode),
+                title: const Text('Appearance'),
+                onTap: () => {Navigator.of(context).pop()},
+              ),
+            ],
           ),
-          ListTile(
-            leading: const Icon(Icons.exit_to_app),
-            title: const Text('Logout'),
-            onTap: () => {Navigator.of(context).pop()},
+          const Divider(),
+          Expanded(
+            child: Align(
+              alignment: Alignment.bottomCenter,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end, //still unable to change alignment
+                children: <Widget>[
+                  ListTile(
+                    leading: const Icon(Icons.exit_to_app),
+                    title: const Text('Logout'),
+                    onTap: () => {Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => SignInScreen()))},
+                  ),
+                  ListTile(
+                    leading: const Icon(Icons.info),
+                    title: const Text('About Us'),
+                    onTap: () => {Navigator.of(context).pop()},
+                  ),
+                ],
+              ),
+            ),
           ),
         ],
       ),
