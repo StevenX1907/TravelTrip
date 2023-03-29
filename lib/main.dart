87619a3f41d9386eb3ca17296618ed085b726551
YapJie <B10923075@gemail.yuntech.edu.tw>
2023-03-29 11:56:34 +0800

    scrren

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Jie',
+      title: 'Flutter ABC',
       theme: ThemeData(
         // This is the theme of your application.
         //
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:travel_trip_application/reusable_widgets/side_menu.dart';
-import 'package:travel_trip_application/screens/indonesia_screen.dart';
-import 'package:travel_trip_application/screens/malaysia_screen.dart';
-import 'package:travel_trip_application/screens/taiwan_screen.dart';
-import 'package:travel_trip_application/screens/vietnam_screen.dart';
+import 'package:travel_trip_application/screens/countryScreens/indonesia_screen.dart';
+import 'package:travel_trip_application/screens/countryScreens/malaysia_screen.dart';
+import 'package:travel_trip_application/screens/countryScreens/taiwan_screen.dart';
+import 'package:travel_trip_application/screens/countryScreens/vietnam_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
