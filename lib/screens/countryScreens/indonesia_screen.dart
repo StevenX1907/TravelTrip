e07a5303dd506191b4488b0bb06ffce11acadac4
YapJie <B10923075@gemail.yuntech.edu.tw>
2023-06-02 10:38:47 +0800

    exchange

--- lib/screens/countryScreens/indonesia_screen.dart
+++ lib/screens/countryScreens/indonesia_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:travel_trip_application/reusable_widgets/side_menu.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
 
 class Indonesia_screen extends StatefulWidget {
   const Indonesia_screen({Key? key}) : super(key: key);
@@ -12,21 +14,15 @@ class _IndonesiaScreenState extends State<Indonesia_screen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.white,
-        drawer: NavDrawer(),
-        appBar: AppBar(
-          title: Text('TravelTrip'),
-        ),
-        body: Center(
-            child: Stack(
-          children: [
-            Positioned(
-                left: 20,
-                top: 20,
-                child: Image(
-                  image: AssetImage('assets/images/Indonesia.jpg'),
-                )),
-          ],
-        )));
+      backgroundColor: Colors.white,
+      drawer: const NavDrawer(),
+      appBar: AppBar(
+        title: const Text('TravelTrip'),
+      ),
+      body: const WebView(
+        initialUrl: 'assets/html/index.html',
+        javascriptMode: JavascriptMode.unrestricted,
+      ),
+    );
   }
 }
