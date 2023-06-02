dc570d6bc0b3bbbcf42468d386b092f94104bb6c
soul <phamtrungkien1120@gmail.com>
2023-06-02 12:21:01 +0800

    about us page

--- lib/screens/about_us_screen.dart
+++ lib/screens/about_us_screen.dart
@@ -4,6 +4,7 @@ import 'package:url_launcher/url_launcher.dart';
 import 'package:flutter/material.dart';
 
 import '../reusable_widgets/dark_mode.dart';
+import '../reusable_widgets/side_menu.dart';
 
 class AboutUsScreen extends StatefulWidget {
   const AboutUsScreen({Key? key}) : super(key: key);
@@ -18,6 +19,7 @@ class _State extends State<AboutUsScreen> {
     final darkModeProvider = Provider.of<DarkModeExample>(context);
     final isDarkMode = darkModeProvider.isDarkMode;
     return Scaffold(
+      drawer: const NavDrawer(),
       appBar: AppBar(
         title: const Text('About Us'),
         backgroundColor: isDarkMode?Colors.black:Color(0xFF306550),
@@ -60,31 +62,44 @@ class _State extends State<AboutUsScreen> {
               ),
             ),
             const SizedBox(height: 20),
+            const Text(
+              'Connect with us:',
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 10),
             Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 IconButton(
                   icon: Image.asset(
-                      'assets/icons/instagram.jfif',
-                       width: 24,height: 24
+                    'assets/icons/instagram.jfif',
+                    width: 24,
+                    height: 24,
                   ),
-                    onPressed: () {
-                      launchUrl('https://www.instagram.com/phamkien2011/');
-                    },
+                  onPressed: () {
+                    launchUrl('https://www.instagram.com/phamkien2011/');
+                  },
                 ),
+                const SizedBox(width: 10),
                 IconButton(
                   icon: Image.asset(
-                      'assets/icons/facebook.png',
-                      width: 24,height: 24
+                    'assets/icons/facebook.png',
+                    width: 24,
+                    height: 24,
                   ),
                   onPressed: () {
                     launchUrl('https://www.facebook.com/phamtrungkien1120/');
                   },
                 ),
+                const SizedBox(width: 10),
                 IconButton(
                   icon: Image.asset(
-                      'assets/icons/github.png',
-                      width: 24,height: 24
+                    'assets/icons/github.png',
+                    width: 24,
+                    height: 24,
                   ),
                   onPressed: () {
                     launchUrl('https://github.com/K2ofsoul/TravelTrip');
@@ -98,7 +113,12 @@ class _State extends State<AboutUsScreen> {
     );
   }
   Widget buildMemberCard(TeamMember member) {
+    final darkModeProvider = Provider.of<DarkModeExample>(context);
+    final isDarkMode = darkModeProvider.isDarkMode;
     return Card(
+      color: isDarkMode?Colors.black: Colors.white60,
+      // shadowColor:  isDarkMode?Colors.black: Colors.grey,
+      // elevation: 0,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
