c6f95098140d505dc9b2f029b365f265b15f23a5
alex <alexstevenn19@gmail.com>
2023-10-06 13:40:20 +0800

    discussion page v1.2

--- lib/screens/discussion_page.dart
+++ lib/screens/discussion_page.dart
@@ -84,7 +84,7 @@ class _DiscussionPageState extends State<DiscussionPage> {
                   'test app',
                   style: TextStyle(
                     fontSize: 16,
-                    color: Colors.black,
+                    color: isDarkMode? Colors.white : Colors.black,
                     fontWeight: FontWeight.w500,
                   ),
                 ),
@@ -183,7 +183,7 @@ class _DiscussionPageState extends State<DiscussionPage> {
                   'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur',
                   style: TextStyle(
                     fontSize: 16,
-                    color: Colors.black,
+                    color: isDarkMode? Colors.white : Colors.black,
                     fontWeight: FontWeight.w500,
                   ),
                 ),
@@ -269,7 +269,7 @@ class _DiscussionPageState extends State<DiscussionPage> {
                   'hi',
                   style: TextStyle(
                     fontSize: 16,
-                    color: Colors.black,
+                    color: isDarkMode? Colors.white : Colors.black,
                     fontWeight: FontWeight.w500,
                   ),
                 ),
@@ -386,7 +386,6 @@ class _DiscussionPageState extends State<DiscussionPage> {
             ),
             ElevatedButton(
               onPressed: () {
-                // Implement your post creation logic here
                 String postText = _postController.text;
                 if (postText.isNotEmpty) {
                   // You can save the post to your data source or perform other actions
