f50fa4f4134fe330c5d2ba7d327efed0679c6dcc
YapJie <B10923075@gemail.yuntech.edu.tw>
2023-11-24 22:11:47 +0800

    update

--- lib/gen_l10n/app_localizations_ms.dart
+++ lib/gen_l10n/app_localizations_ms.dart
@@ -71,62 +71,6 @@ class AppLocalizationsMs extends AppLocalizations {
   String get aboutUs => 'Tentang Kita';
 
   @override
-  String get MountKinabaluScreen => 'Gunung Kinabalu';
-
-  @override
-  String get MountKinabaluDescription => 'Gunung tertinggi di Borneo, Gunung Kinabalu adalah pemandangan yang mencolok untuk dilihat. Para pendaki suka menikmati jejak yang mengesankan yang memungkinkan melihat pemandangan indah di sekitar wilayah itu. Sangat popular di kalangan orang yang suka pengembaraan, ia adalah salah satu tempat terbaik untuk dilawati di Malaysia bagi pengalaman penuh aksi. Di antara tempat-tempat terbaik untuk dilawati di Malaysia, ini adalah yang teratas.';
-
-  @override
-  String get MountKinabaluAddress => 'Alamat: Gunung Kinabalu, Malaysia';
-  @override
-  String get MantananiScreen => 'Mantanani';
-
-  @override
-  String get MantananiDescription => 'Kepulauan Mantanani di Sabah yang terletak berdekatan dengan Kota Belud merupakan antara tempat pelancongan paling terkenal di Malaysia yang menawarkan tapak menyelam terbaik di Borneo hanya dalam masa sejam dari Kota Kinabalu. Ia merupakan salah satu tempat paling terkenal di Malaysia disebabkan tapak menyelamnya dan menjadi pilihan utama bagi semua penyelam dan penunggang selam yang berduyun-duyun ke pulau ini dalam lawatan sehari dari ibu negara. Dengan jumlah yang ramai, penyelam dari serata dunia datang ke pulau-pulau ini untuk mengembara di air yang jernih dan bertemu dengan kuda laut dan pari. Penyelam yang mendapat pengiktirafan scuba boleh memilih dari lebih daripada 20 lokasi di sini untuk sukan air di Malaysia.';
-
-  @override
-  String get MantananiAddress => 'Alamat: Mantanani, Malaysia';
-  @override
-  String get LangkawiScreen => 'Pulau Langkawi';
-  @override
-  String get LangkawiDescription => 'Pulau Langkawi merupakan destinasi yang terkenal dengan pantai berpasir yang luas dan air biru kehijauan yang memikat para pelancong. Geopark Langkawi merupakan tapak warisan UNESCO yang menarik banyak pengunjung dan tanpa ragu merupakan antara tempat yang mesti dilawati di Malaysia. Ia adalah manifestasi yang sempurna dari warisan geologi di kawasan tersebut. Ia adalah salah satu tempat terbaik untuk dilawati di Malaysia bersama keluarga.';
-  @override
-  String get LangkawiAddress => 'Pulau Langkawi, Malaysia';
-  @override
-  String get MovHotels => 'Hotel Mov';
-
-  @override
-  String get MovHotelsDescription => 'Terletak di tengah Bukit Bintang, hotel ini memberikan akses mudah ke berbagai tempat makanan, pusat perbelanjaan, dan jalan makanan yang menarik. Dengan bilik-bilik moden dan menarik secara estetika, kolam renang atap yang menakjubkan dengan pemandangan bandar, serta kakitangan yang mesra, ia merupakan pilihan yang sempurna untuk penginapan yang selesa dan mudah.';
-  @override
-  String get SunwayPutraHotel => 'Hotel Sunway Putra';
-
-  @override
-  String get SunwayPutraHotelDescription => 'Hotel yang menawan yang berhubung dengan pusat membeli-belah, menawarkan akses yang mudah ke pelbagai tempat makan dan pengangkutan awam. Memiliki bilik yang luas dan bersih dengan peningkatan percuma yang tersedia, pemandangan yang mengagumkan, dan kedai kopi yang selesa di tempat. Lokasi yang cemerlang berhampiran Stesen PWTC untuk menjelajah bandar yang mudah.';
-  @override
-  String get PacificSuteraHotel => 'Hotel Pacific Sutera';
-
-  @override
-  String get PacificSuteraHotelDescription => 'Resor mewah dengan pemandangan laut dan taman yang indah. Bilik yang bersih dan elegan dengan sentuhan tempatan. Memiliki pantai peribadi, padang golf, kolam renang, dan taman permainan kanak-kanak. Pilihan teratas untuk makan malam dengan pelbagai restoran, termasuk Al Fresco, Silk Garden, dan Cafe Boleh. Penginapan yang berkesan, staf yang membantu.';
-  @override
-  String get EnakKL => 'Enak KL';
-
-  @override
-  String get EnakKLDescription => 'Enak adalah sebuah restoran Melayu mewah yang terletak di Starhill Gallery yang sangat mewah di Kuala Lumpur. Sebagai salah satu tempat makan terbaik di ibu kota, seseorang dapat mencoba hidangan Melayu paling lezat, banyak di antaranya berdasarkan resep yang turun-temurun dalam keluarga Sherena Razaly, pemilik restoran. Walaupun rasanya semuanya tradisional dalam hidangan khas seperti Ayam Goreng Lengkuas yang merupakan ayam goreng dengan halia biru, Enak tanpa ragu adalah restoran Malaysia terbaik di Kuala Lumpur dan wajib dikunjungi ketika Anda berada di kota ini.';
-  @override
-  String get RestoranSamy => 'Restoran Samy';
-
-  @override
-  String get RestoranSamyDescription => 'Mencari restoran India terbaik di Malaysia? Lawati Restoran Samy di Ipoh jika anda ingin mencuba beberapa hidangan India yang autentik. Restoran ini setaraf dengan Devi’s Corner di Kuala Lumpur dan setelah berjaya selama lebih dari 20 tahun, Restoran Samy menawarkan nasi daun pisang yang superb, dan hidangan vegetarian serta non-vegetarian yang menakjubkan.';
-  @override
-  String get LeQueRestaurant => 'Restoran Le Que';
-
-  @override
-  String get LeQueRestaurantDescription => 'Ditubuhkan pada tahun 2014, Restoran Le Que mungkin agak baru dibandingkan dengan banyak restoran lain di seluruh negara, tetapi anda boleh sentiasa mengharapkan hidangan yang lazat disediakan oleh jurumasak yang terlatih dan berpengalaman. Ia merupakan sebuah kafe fusi di mana anda boleh menikmati pelbagai masakan Barat dan pilihan pasta yang baik. Jika anda berada di bandar Melaka dan mempunyai selera untuk pasta, anda haruslah ke Le Que.';
-
-}
-
-
-  @override
   String get posts => 'Jawatan';
 
   @override
@@ -317,10 +261,6 @@ class AppLocalizationsMs extends AppLocalizations {
   @override
   String get wulaiDescription => 'Wulai, terletak di pegunungan subur di Taiwan utara, adalah desa adat indah yang terkenal dengan keindahan alam, kekayaan budaya, dan sumber air panas yang menenangkan. Sebagai bagian dari tanah leluhur suku Atayal, Wulai menawarkan gambaran unik tentang warisan adat Taiwan. Desa ini dikelilingi oleh hutan lebat dan Sungai Nanshih yang berkilauan, menciptakan latar belakang yang tenang bagi pengunjung yang mencari ketenangan dari kehidupan perkotaan. Salah satu daya tarik utama Wulai adalah sumber air panasnya yang meremajakan, yang menarik penduduk lokal dan wisatawan untuk mencari relaksasi. Perairan panas bumi diyakini memiliki khasiat penyembuhan, sehingga berendam di pemandian terbuka menjadi pengalaman yang terapeutik dan mendalam. Pengunjung juga dapat menjelajahi Air Terjun Wulai yang indah, mengalir secara dramatis dari ketinggian 80 meter, dapat diakses melalui jalan setapak berpemandangan indah yang melintasi hutan hijau. Wulai bukan hanya surga bagi pecinta alam; ini juga menawarkan gambaran sekilas tentang cara hidup masyarakat adat. Museum Adat Atayal memberikan wawasan komprehensif tentang tradisi suku, memamerkan artefak, pakaian tradisional, dan pameran interaktif yang menceritakan kekayaan sejarah budaya masyarakat Atayal.';
 
-  // @override
-  // String get malaysia => 'Malaysia';
-
-
 
   String get grandTaipei => 'Grand Hyatt Taipei';
   @override
@@ -353,6 +293,58 @@ class AppLocalizationsMs extends AppLocalizations {
   String get matsuContact => 'Hubungi: +886 2 2345 8833';
   @override
   String get matsuAddress => 'Alamat: No.19, Songgao Road 6F, A4, Xintiandi, Shin Kong Mitsukoshi Xinyi, Distrik Xinyi, Taipei 110 Taiwan';
+  @override
+  String get MountKinabaluScreen => 'Gunung Kinabalu';
+
+  @override
+  String get MountKinabaluDescription => 'Gunung tertinggi di Borneo, Gunung Kinabalu adalah pemandangan yang mencolok untuk dilihat. Para pendaki suka menikmati jejak yang mengesankan yang memungkinkan melihat pemandangan indah di sekitar wilayah itu. Sangat popular di kalangan orang yang suka pengembaraan, ia adalah salah satu tempat terbaik untuk dilawati di Malaysia bagi pengalaman penuh aksi. Di antara tempat-tempat terbaik untuk dilawati di Malaysia, ini adalah yang teratas.';
+
+  @override
+  String get MountKinabaluAddress => 'Alamat: Gunung Kinabalu, Malaysia';
+  @override
+  String get MantananiScreen => 'Mantanani';
+
+  @override
+  String get MantananiDescription => 'Kepulauan Mantanani di Sabah yang terletak berdekatan dengan Kota Belud merupakan antara tempat pelancongan paling terkenal di Malaysia yang menawarkan tapak menyelam terbaik di Borneo hanya dalam masa sejam dari Kota Kinabalu. Ia merupakan salah satu tempat paling terkenal di Malaysia disebabkan tapak menyelamnya dan menjadi pilihan utama bagi semua penyelam dan penunggang selam yang berduyun-duyun ke pulau ini dalam lawatan sehari dari ibu negara. Dengan jumlah yang ramai, penyelam dari serata dunia datang ke pulau-pulau ini untuk mengembara di air yang jernih dan bertemu dengan kuda laut dan pari. Penyelam yang mendapat pengiktirafan scuba boleh memilih dari lebih daripada 20 lokasi di sini untuk sukan air di Malaysia.';
+
+  @override
+  String get MantananiAddress => 'Alamat: Mantanani, Malaysia';
+  @override
+  String get LangkawiScreen => 'Pulau Langkawi';
+  @override
+  String get LangkawiDescription => 'Pulau Langkawi merupakan destinasi yang terkenal dengan pantai berpasir yang luas dan air biru kehijauan yang memikat para pelancong. Geopark Langkawi merupakan tapak warisan UNESCO yang menarik banyak pengunjung dan tanpa ragu merupakan antara tempat yang mesti dilawati di Malaysia. Ia adalah manifestasi yang sempurna dari warisan geologi di kawasan tersebut. Ia adalah salah satu tempat terbaik untuk dilawati di Malaysia bersama keluarga.';
+  @override
+  String get LangkawiAddress => 'Pulau Langkawi, Malaysia';
+  @override
+  String get MovHotels => 'Hotel Mov';
+
+  @override
+  String get MovHotelsDescription => 'Terletak di tengah Bukit Bintang, hotel ini memberikan akses mudah ke berbagai tempat makanan, pusat perbelanjaan, dan jalan makanan yang menarik. Dengan bilik-bilik moden dan menarik secara estetika, kolam renang atap yang menakjubkan dengan pemandangan bandar, serta kakitangan yang mesra, ia merupakan pilihan yang sempurna untuk penginapan yang selesa dan mudah.';
+  @override
+  String get SunwayPutraHotel => 'Hotel Sunway Putra';
+
+  @override
+  String get SunwayPutraHotelDescription => 'Hotel yang menawan yang berhubung dengan pusat membeli-belah, menawarkan akses yang mudah ke pelbagai tempat makan dan pengangkutan awam. Memiliki bilik yang luas dan bersih dengan peningkatan percuma yang tersedia, pemandangan yang mengagumkan, dan kedai kopi yang selesa di tempat. Lokasi yang cemerlang berhampiran Stesen PWTC untuk menjelajah bandar yang mudah.';
+  @override
+  String get PacificSuteraHotel => 'Hotel Pacific Sutera';
+
+  @override
+  String get PacificSuteraHotelDescription => 'Resor mewah dengan pemandangan laut dan taman yang indah. Bilik yang bersih dan elegan dengan sentuhan tempatan. Memiliki pantai peribadi, padang golf, kolam renang, dan taman permainan kanak-kanak. Pilihan teratas untuk makan malam dengan pelbagai restoran, termasuk Al Fresco, Silk Garden, dan Cafe Boleh. Penginapan yang berkesan, staf yang membantu.';
+  @override
+  String get EnakKL => 'Enak KL';
+
+  @override
+  String get EnakKLDescription => 'Enak adalah sebuah restoran Melayu mewah yang terletak di Starhill Gallery yang sangat mewah di Kuala Lumpur. Sebagai salah satu tempat makan terbaik di ibu kota, seseorang dapat mencoba hidangan Melayu paling lezat, banyak di antaranya berdasarkan resep yang turun-temurun dalam keluarga Sherena Razaly, pemilik restoran. Walaupun rasanya semuanya tradisional dalam hidangan khas seperti Ayam Goreng Lengkuas yang merupakan ayam goreng dengan halia biru, Enak tanpa ragu adalah restoran Malaysia terbaik di Kuala Lumpur dan wajib dikunjungi ketika Anda berada di kota ini.';
+  @override
+  String get RestoranSamy => 'Restoran Samy';
+
+  @override
+  String get RestoranSamyDescription => 'Mencari restoran India terbaik di Malaysia? Lawati Restoran Samy di Ipoh jika anda ingin mencuba beberapa hidangan India yang autentik. Restoran ini setaraf dengan Devi’s Corner di Kuala Lumpur dan setelah berjaya selama lebih dari 20 tahun, Restoran Samy menawarkan nasi daun pisang yang superb, dan hidangan vegetarian serta non-vegetarian yang menakjubkan.';
+  @override
+  String get LeQueRestaurant => 'Restoran Le Que';
+
+  @override
+  String get LeQueRestaurantDescription => 'Ditubuhkan pada tahun 2014, Restoran Le Que mungkin agak baru dibandingkan dengan banyak restoran lain di seluruh negara, tetapi anda boleh sentiasa mengharapkan hidangan yang lazat disediakan oleh jurumasak yang terlatih dan berpengalaman. Ia merupakan sebuah kafe fusi di mana anda boleh menikmati pelbagai masakan Barat dan pilihan pasta yang baik. Jika anda berada di bandar Melaka dan mempunyai selera untuk pasta, anda haruslah ke Le Que.';
 
 }
 
